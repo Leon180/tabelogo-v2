1f884a9c4702a0044b953e0f35f77fb512a44926
Leon Li <lioung860302@gmail.com>
2025-12-24 23:57:14 +0800

    fix(frontend): add Google Places API image domains to Next.js config
    Fixed issue where restaurant photos from Google Places API were not
    displaying in the frontend.
    
    Added remotePatterns configuration to allow:
    - places.googleapis.com (Places API photos)
    - *.googleusercontent.com (Google CDN)
    
    This enables Next.js Image component to load and optimize images
    from Google Places API.
    
    Fixes: Images not displaying in PlaceDetailModal

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'places.googleapis.com',
+        pathname: '/v1/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
